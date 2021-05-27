f5a7b97f471e90f0e14667d47ce7f923c74275dc
Veralalala <15951908185@163.com>
2021-05-27 17:37:06 +0800

    1

---
+++ springboot_grpc/grpc_lib/src/main/java/com/fs/grpc/grpc_lib/GrpcLibApplication.java
@@ -0,0 +1,13 @@
+package com.fs.grpc.grpc_lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcLibApplication.class, args);
+    }
+
+}
