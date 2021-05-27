cc010435c607a76784cf3379e7cd51a76c0f985f
Veralalala <15951908185@163.com>
2021-05-27 17:34:44 +0800

    1

---
+++ springboot_grpc/src/main/java/com/fs/grpc/springboot_grpc/SpringbootGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.fs.grpc.springboot_grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGrpcApplication.class, args);
+    }
+
+}
